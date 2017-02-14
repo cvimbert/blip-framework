dac4f2c28006011c0658af7b7dd216a607c45447
Christophe Vimbert <c.vimbert@outlook.fr>
2017-02-14 18:13:27 +0100

    Event dispatcher dev

--- src/core/common/event-dispatcher.class.ts
+++ src/core/common/event-dispatcher.class.ts
@@ -4,8 +4,35 @@
 
 export class EventDispatcher {
 
+    private _eventCallbacks:{[key:string]:Function[]} = {};
+
     dispatchEvent(eventType:string, param:any = null) {
-        
+        if (this._eventCallbacks[eventType]) {
+            this._eventCallbacks[eventType].forEach((callback:Function) => {
+                callback(param);
+            });
+        }
     }
 
+    on(eventType:string, callback:Function) {
+
+        if (!this._eventCallbacks[eventType]) {
+            this._eventCallbacks[eventType] = [];
+        }
+
+        this._eventCallbacks[eventType].push(callback);
+    }
+
+    off(eventType:string, callback:Function = null) {
+
+        if (!callback) {
+            this._eventCallbacks[eventType] = [];
+        } else {
+            let index:number = this._eventCallbacks[eventType].indexOf(callback);
+
+            if (index !== -1) {
+                this._eventCallbacks[eventType].splice(index, 1);
+            }
+        }
+    }
 }
