c6a83b1cb2013973927ff983b2c490fc5988954e
Christophe <christophe@tralalere.com>
2017-04-04 09:56:24 +0200

    Creating typedoc documentation

--- core/src/common/time-utils.class.ts
+++ core/src/common/time-utils.class.ts
@@ -1,14 +1,14 @@
 /**
  * Created by Christophe on 03/02/2017.
  */
-import {Observable} from "rxjs/Rx";
+//import {Observable} from "rxjs/Rx";
 
 // tout ça est à voir plus tard
 
 export class TimeUtils {
 
-    static setInterval(time:number = 0, iterations:number = 1):Observable<number> {
+    /*static setInterval(time:number = 0, iterations:number = 1):Observable<number> {
 
         return null;
-    }
+    }*/
 }
---
+++ core/src/files/file.class.js
@@ -0,0 +1,13 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+/**
+ * Created by Christophe on 01/02/2017.
+ */
+var File = (function () {
+    function File(path) {
+        this.path = path;
+    }
+    return File;
+}());
+exports.File = File;
+//# sourceMappingURL=file.class.js.map
