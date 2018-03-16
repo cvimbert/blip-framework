fb71313afe59b22a3a2d8418133674b5b69045f8
Christophe <ch.vimbert@gmail.com>
2018-03-16 18:12:42 +0100

    Working on objects definitions

--- code-analysis/test.ts
+++ code-analysis/test.ts
@@ -1,6 +1,6 @@
 import {BlpParser} from "../blp-parser/blp-parser.class";
 import {CodeStructure} from "../blp-parser/interfaces/assertions.interface";
-import {GameObjectDefinition} from "../core/src/providers/game-object-definition.class";
-import {CodeFileLoader} from "../core/src/providers/code-file-loader.class";
+import {GameObjectDefinition} from "../core/src/definitions/game-object-definition.class";
+import {CodeFileLoader} from "../core/src/definitions/code-file-loader.class";
 
 let loader: CodeFileLoader = new CodeFileLoader("../examples/blp/object1.blp");
